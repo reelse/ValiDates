3b00967d613bb83958d75ab06040949cbc5c8961
Orion Schemenauer <oschemenauer@gmail.com>
2025-05-01 13:09:36 -0400

    hoist motion

--- vite.config.js
+++ vite.config.js
@@ -18,12 +18,13 @@ export default defineConfig({
     rollupOptions: {
       // make sure to externalize deps that shouldn't be bundled
       // into your library
-      external: ['react'],
+      external: ['react', 'motion'],
       output: {
         // Provide global variables to use in the UMD build
         // for externalized deps
         globals: {
           react: 'React',
+          motion: 'motion',
         },
       },
     },
